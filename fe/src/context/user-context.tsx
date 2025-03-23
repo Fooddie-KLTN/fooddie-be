3def491a1a4917b7d06b1c66cef718e3cf54c3d1
tonyanhkhoi <tonysquadgamer@gmail.com>
2025-03-24 04:54:31 +0700

    <dev> add store admin

--- fe/src/context/user-context.tsx
+++ fe/src/context/user-context.tsx
@@ -2,20 +2,17 @@ import { createContext, useContext, ReactNode } from 'react';
 
 // Define Permission type based on backend permissions
 export type Permission =
-  | 'create_role' | 'edit_role' | 'view_role' | 'delete_role' | 'all_role'
-  | 'create_user' | 'write_user' | 'read_user' | 'delete_user' | 'all_user'
-  | 'create_course' | 'write_course' | 'read_course' | 'delete_course' | 'all_course'
-  | 'create_exam' | 'write_exam' | 'read_exam' | 'delete_exam' | 'all_exam'
-  | 'create_review' | 'write_review' | 'read_review' | 'delete_review' | 'all_review'
-  | 'create_order' | 'write_order' | 'read_order' | 'delete_order' | 'all_order'
-  | 'create_lecturer' | 'write_lecturer' | 'read_lecturer' | 'delete_lecturer' | 'all_lecturer'
-  | 'create_content' | 'write_content' | 'read_content' | 'delete_content' | 'all_content'
-  | 'create_tag' | 'write_tag' | 'read_tag' | 'delete_tag' | 'all_tag'
-  | 'create_topic' | 'write_topic' | 'read_topic' | 'delete_topic' | 'all_topic'
-  | 'create_chapter' | 'write_chapter' | 'read_chapter' | 'delete_chapter' | 'all_chapter'
-  | 'create_activation_code' | 'write_activation_code' | 'read_activation_code' | 'delete_activation_code' | 'all_activation_code'
-  | 'create_student_group' | 'write_student_group' | 'read_student_group' | 'delete_student_group' | 'all_student_group'
-  | 'create_promotion' | 'write_promotion' | 'read_promotion' | 'delete_promotion' | 'all_promotion';
+'create_role' | 'edit_role' | 'view_role' | 'delete_role' | 'all_role'|
+'create_user' | 'write_user' | 'read_user' | 'delete_user' | 'all_user'|
+'create_rule' | 'write_rule' | 'read_rule' | 'delete_rule' | 'all_rule'|
+'create_store' | 'write_store' | 'read_store' | 'delete_store' | 'all_store'|
+'create_category' | 'write_catgory' | 'read_category' | 'delete_category' | 'all_category'|
+'create_order' | 'write_order' | 'read_order' | 'delete_order' | 'all_order'|
+'create_shipper' | 'write_shipper' | 'read_shipper' | 'delete_shipper' | 'all_shipper'|
+'create_dashboard' | 'write_dashboard' | 'read_dashboard' | 'delete_dashboard' | 'all_dashboard'|
+'create_promotion' | 'write_promotion' | 'read_promotion' | 'delete_promotion' | 'all_promotion'|
+'create_event' | 'write_event' | 'read_event' | 'delete_event' | 'all_event';
+
 
 // Context type
 interface UserContextType {
