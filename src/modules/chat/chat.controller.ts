96f38104d284492be8861063aa6b56981373b14a
tonyanhkhoi <tonysquadgamer@gmail.com>
2025-06-20 19:54:48 +0700

    <dev> update chat

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,20 +1,21 @@
-import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpException, HttpStatus, UseGuards, Req, Get } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { FoodService } from '../food/food.service';
 
 @Controller('chat')
 export class ChatController {
-  constructor(private readonly chatService: ChatService) {}
+  constructor(private readonly chatService: ChatService, private readonly foodService: FoodService,) {}
 
+  @UseGuards(AuthGuard)
   @Post()
-  async handleChat(@Body('userMessage') userMessage: string) {
-    try {
-      const reply = await this.chatService.generateReply(userMessage);
-      return { reply };
-    } catch (error) {
-      throw new HttpException(
-        { message: error.message || 'Lỗi hệ thống khi xử lý chat' },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  async handleChat(@Body('userMessage') userMessage: string, @Req() req) {
+    const userId = req.user?.id;
+    if (!userId) throw new Error('User ID not found in token');
+    console.log('[ChatController] Received message:', userMessage);
+    console.log('[ChatController] userId:', userId);
+    const reply = await this.chatService.generateReply(userMessage, userId);
+    return { reply };
   }
+
 }
--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { FoodService } from '../food/food.service';
+import { OrderService } from '../order/order.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Food } from 'src/entities/food.entity';
+import { Order } from 'src/entities/order.entity';
+import { Restaurant } from 'src/entities/restaurant.entity';
+import { Category } from 'src/entities/category.entity';
+import { Review } from 'src/entities/review.entity';
+import { GoogleCloudStorageService } from 'src/gcs/gcs.service';
+import { OrderDetail } from 'src/entities/orderDetail.entity';
+import { User } from 'src/entities/user.entity';
+import { Address } from 'src/entities/address.entity';
+import { Promotion } from 'src/entities/promotion.entity';
+import { Checkout } from 'src/entities/checkout.entity';
+import { PromotionService } from '../promotion/promotion.service';
+import { PendingAssignmentService } from 'src/pg-boss/pending-assignment.service';
+import { PendingShipperAssignment } from 'src/entities/pendingShipperAssignment.entity';
+import { QueueService } from 'src/pg-boss/queue.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Food, Order, Restaurant, Category, Review, OrderDetail, User, Address, Promotion, Checkout, PendingShipperAssignment])], 
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, JwtModule, FoodService, OrderService, GoogleCloudStorageService, PromotionService, PendingAssignmentService, QueueService, JwtService],
 })
 export class ChatModule {}
